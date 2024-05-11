3ed2d63821e2d9fe8855a43ec42bef75dfca5dbc
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-05-11 22:13:39 +0530

    i

--- lemarche/build.sh
+++ lemarche/build.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
+# Display message indicating the start of the build process
 echo "Building the project..."
+
+# Install Python dependencies from requirements.txt
 python3.9 -m pip install -r requirements.txt
 
+# Run Django migrations
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
+# Collect static files
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
