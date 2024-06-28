0dde4ce52deb3bb7912f9dcfbbb399bfe520c773
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-06-29 00:15:23 +0530

    channels configured 0.21

--- lemarche/lemarche/settings.py
+++ lemarche/lemarche/settings.py
@@ -52,15 +52,15 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = "lemarche.asgi.application"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
