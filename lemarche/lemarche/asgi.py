f99056a7334b50ba9367e2b52508132f70ed91ce
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-06-28 23:39:32 +0530

    channels configured 0.17

--- lemarche/lemarche/asgi.py
+++ lemarche/lemarche/asgi.py
@@ -1,14 +1,17 @@
 import os
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-import core.routing
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lemarche.settings")
 print(f'DJANGO_SETTINGS_MODULE: {os.environ.get("DJANGO_SETTINGS_MODULE")}')
 django.setup()
+
+import core.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
